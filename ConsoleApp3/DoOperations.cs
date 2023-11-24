519f900a7207855a974b74ff146444cfd85cb33c
el.petryaeva <el.petryaeva@mail.ru>
2023-11-24 16:19:22 +0500

    lab1 и lab2 -приоритет операций -выполнение операций -считывание выражения

---
+++ ConsoleApp3/DoOperations.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp3;
+
+public class DoOperations
+{
+
+    // Выполнение операции
+    public double Calculate(double num1, double num2, char operation)
+    {
+        switch (operation)
+        {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) throw new DivideByZeroException("Делить на ноль нельзя");
+                return num1 / num2;
+            default:
+                throw new ArgumentException("Неподдерживаемый оператор");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp3;
+
+// инициализация списков символов и чисел выражения 
+List<char> operations = new List<char>();
+List<double> numbers = new List<double>();
+// список возможных вводимых символов
+List<char> operationsList = new List<char>{'-', '+', '/', '*'};
+
+Console.WriteLine("Введите математическое выражение");
+string mathematicalExpression = Console.ReadLine();
+
+// разделяем введенную строку на части, используя операторы
+string[] parts = mathematicalExpression.Split(new char[] { '+', '-', '*', '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var element in parts)
+{
+    // проверяем может ли элемент быть числом
+    if (double.TryParse(element, out double number))
+    {
+        // добавляем число в массив чисел
+        numbers.Add(number);
+    }
+} 
+
+foreach (char element in mathematicalExpression)
+{
+    // цикл для сравнения значений строки с массивом операций
+    foreach (var symbol in operationsList)
+    {
+        // проверяем может ли элемент быть операцией
+        if (element == symbol)
+        {
+            operations.Add(symbol);
+        }
+    }
+}
+
+// лабораторная работа 2
+
+DoOperations doOperations = new DoOperations();
+
+// сначала выполняются операции выше по приоритету
+for (int i = 0; i < operations.Count; i++)
+{
+    if (operations[i] == '*' || operations[i] == '/')
+    {
+        // передаем в метод Calculate две переменные и операцию
+        // записываем результат в том же массиве чисел
+        numbers[i] = doOperations.Calculate(numbers[i], numbers[i + 1], operations[i]);
+        // удаляем использованную для подсчета переменную
+        numbers.RemoveAt(i + 1);
+        // удаляем использованный для подсчета оператор
+        operations.RemoveAt(i);
+        i--;
+    }
+}
+
+// выполнение остальных операций
+for (int i = 0; i < operations.Count; i++)
+{
+    if (operations.Contains(operations[i]))
+    {
+        // передаем в метод Calculate две переменные и операцию
+        // записываем результат в том же массиве чисел
+        numbers[i] = doOperations.Calculate(numbers[i], numbers[i + 1], operations[i]);
+        // удаляем использованную для подсчета переменную
+        numbers.RemoveAt(i + 1);
+        // удаляем использованный для подсчета оператор
+        operations.RemoveAt(i);
+        i--;
+    }
+}
+Console.WriteLine(numbers[0]);
